73b8ac8b13e5533b0b7e2ff18ea790337f73a8fb
Luke <burrisl@oregonstate.edu>
2019-11-28 10:10:59 -0500

    Update ftclient.py

--- ftclient.py
+++ ftclient.py
@@ -4,29 +4,29 @@ import sys
 import signal
 
 # Shows the user the correct usage and then exits with code 1
-def printUsage():
+def printUsage(sys.argv):
 	print 'USAGE: %s [serverName] [serverPort] [command (-l or -g)] [fileName (optional if command is -l)] [clientPort]' % argv[0]
 	exit(1)
 
 # Get necessary variables from the command line
 if (len(sys.argv) < 5 or len(sys.argv) > 6):
-	printUsage()
+	printUsage(sys.argv)
 
 # Check serverName
 if (sys.argv[1] != 'flip1' and sys.argv[1] != 'flip2' and sys.argv[1] != 'flip3'): 
-	printUsage()
+	printUsage(sys.argv)
 else:
 	serverName = sys.argv[1] + '.engr.oregonstate.edu'
 
 # Check serverPort
 if (not(sys.argv[2].isnumeric())):
-	printUsage()
+	printUsage(sys.argv)
 else:
 	serverPort = int(sys.argv[2])
 
 # Check command
 if (sys.argv[3] != '-l' and sys.argv[3] != '-g'):
-	printUsage()
+	printUsage(sys.argv)
 else:
 	command = sys.argv[3]
 
@@ -35,7 +35,7 @@ else:
 if (sys.argv[3] == '-l'):
 	# Check clientPort in position 4
 	if (not(sys.argv[4].isnumeric())):
-		printUsage()
+		printUsage(sys.argv)
 	else:
 		clientPort = sys.argv[4]
 # Command was -g so fileName is in sys.argv[4] and clientPort should be in sys.argv[5]
@@ -43,7 +43,7 @@ else:
 	fileName = sys.argv[4]
 	# Check clientPort in position 5
 	if (not(sys.argv[5].isnumeric())):
-		printUsage()
+		printUsage(sys.argv)
 	else:
 		clientPort = sys.argv[5]
 

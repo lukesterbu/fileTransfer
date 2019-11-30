e972997232337ebd1b2cdc3e239481765b931b4a
Luke <burrisl@oregonstate.edu>
2019-11-30 10:26:47 -0500

    Update ftclient.py

--- ftclient.py
+++ ftclient.py
@@ -92,6 +92,7 @@ if (command == '-l'):
 elif (command == '-g'):
 	sock.send(fileName)
 	serverMessage = sock.recv(2048)
+	print serverMessage
 	if (serverMessage == "FILE NOT FOUND"):
 		print shortServerName + ":" + str(serverPort) + " says " + serverMessage
 	else:

59d10cb7c1c34b918a302f0a0b1117ccf66d1d13
Luke <burrisl@oregonstate.edu>
2019-12-01 18:01:24 -0500

    Update ftclient.py

--- client/ftclient.py
+++ client/ftclient.py
@@ -117,7 +117,7 @@ elif (command == '-g'):
 		fileContents = ""
 		# Loop until the entire file has been received
 		while (totalRead <= fileLength):
-			print totalRead + " = " + long(fileLength)
+			print str(totalRead) + " = " + str(fileLength)
 			fileContents = fileContents + sock.recv(recvSize)
 			totalRead = len(fileContents)
 		# Create the file if it doesn't exist
